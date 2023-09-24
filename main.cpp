22b5f14b390e47200f22ee337199bdccaaaadb5e
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2023-09-24 13:46:19 +0300

    Updates

--- main.cpp
+++ main.cpp
@@ -7,18 +7,20 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
     // Convert date to Julian days
-    double Y, M, D, H, m, s, B, A, Z, T, JD, U, L0, 
+    double Y, M, D, H, m, s, B, A, Z, T, JD, U, L0, h,
     ET1000, ET, DELTA, SF, LONG, LAT, FAJR_ANGLE, ISHA_ANGLE;
-    Y = 2023;
+    Y = 2020;
     M = 9;
-    D = 23;
+    D = 6;
     H = 12;
-    m = 1;
-    s = 1;
-    LAT = 29.8403;
-    LONG = 31.2982;
-    FAJR_ANGLE = 19.5;
-    ISHA_ANGLE = 17.5;
+    m = 0;
+    s = 0;
+    Z = +7;
+    SF = 1;
+    LAT = -6.2;
+    LONG = 106.816667;
+    FAJR_ANGLE = 20;
+    ISHA_ANGLE = 18;
 
     if(INT(M) <= 2) {
         M += 12;
@@ -26,25 +28,25 @@ int main (int argc, char *argv[]) {
     }
     A = INT(Y / 100);
     B = 2 + INT(A / 4) - A;
-    Z = +2;
+    h = 8;
 
     JD = 1720994.5 + INT(365.25 * Y) + INT(30.6001 * (M + 1))
         + B + D + (H * 3600 + m * 60 + s) / 86400.0 - Z / 24.0;
 
     // Calculate Sun declination
     T = 2 * PI * (JD - 2451545) / 365.25;
-    DELTA = 0.37877 + 23.264 * sin(57.297 * T - 79.547) + 0.3812 * 
-        sin(2 * 57.297 * T - 82.682) + 0.17132 * sin(3 * 57.297 * T - 59.722);
+    DELTA = 0.37877 + 23.264 * sin((57.297 * T - 79.547) * PI/180) + 0.3812 * 
+        sin((2 * 57.297 * T - 82.682) * PI/180) + 0.17132 * sin((3 * 57.297 * T - 59.722) * PI/180);
 
     // Calculate equation of time
     U = (JD - 2451545) / 36525;
     L0 = 280.46607 + 36000.7698 * U;
-    ET1000 = -(1789 + 237*U) * sin(L0) -
-	(7146 - 62*U) * cos(L0) + 
-	(9934 - 14*U) * sin(2*L0) - 
-	(29 + 5*U) * cos(2*L0) +
-	(74 + 10*U) * sin(3*L0) +
-	(320 - 4*U) * cos(3*L0) -
-	212 * sin(4*L0);
+    ET1000 = -(1789 + 237*U) * sin((L0) * PI/180) -
+	(7146 - 62*U) * cos((L0) * PI/180) + 
+	(9934 - 14*U) * sin((2*L0) * PI/180) - 
+	(29 + 5*U) * cos((2*L0) * PI/180) +
+	(74 + 10*U) * sin((3*L0) * PI/180) +
+	(320 - 4*U) * cos((3*L0) * PI/180) -
+	212 * sin((4*L0) * PI/180);
     ET = ET1000 / 1000;
 }
