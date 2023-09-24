e75ab4c68440e7487b0e9b7b624ccd7c58f3af28
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2023-09-24 13:53:28 +0300

    Updates

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #define INT(x) ((int) (x))
 #define PI 3.141592653589793238462643383279502884197
+#define SIN(x) sin((x) * PI/180)
+#define COS(x) cos((x) * PI/180)
+#define TAN(x) tan((x) * PI/180)
+#define ACOT(x) acot((x) * PI/180)
 
 using namespace std;
 
@@ -35,18 +39,18 @@ int main (int argc, char *argv[]) {
 
     // Calculate Sun declination
     T = 2 * PI * (JD - 2451545) / 365.25;
-    DELTA = 0.37877 + 23.264 * sin((57.297 * T - 79.547) * PI/180) + 0.3812 * 
-        sin((2 * 57.297 * T - 82.682) * PI/180) + 0.17132 * sin((3 * 57.297 * T - 59.722) * PI/180);
+    DELTA = 0.37877 + 23.264 * SIN(57.297 * T - 79.547) + 0.3812 * 
+        SIN(2 * 57.297 * T - 82.682) + 0.17132 * SIN(3 * 57.297 * T - 59.722);
 
     // Calculate equation of time
     U = (JD - 2451545) / 36525;
     L0 = 280.46607 + 36000.7698 * U;
-    ET1000 = -(1789 + 237*U) * sin((L0) * PI/180) -
-	(7146 - 62*U) * cos((L0) * PI/180) + 
-	(9934 - 14*U) * sin((2*L0) * PI/180) - 
-	(29 + 5*U) * cos((2*L0) * PI/180) +
-	(74 + 10*U) * sin((3*L0) * PI/180) +
-	(320 - 4*U) * cos((3*L0) * PI/180) -
-	212 * sin((4*L0) * PI/180);
+    ET1000 = -(1789 + 237*U) * SIN(L0) -
+	(7146 - 62*U) * COS(L0) + 
+	(9934 - 14*U) * SIN(2*L0) - 
+	(29 + 5*U) * COS(2*L0) +
+	(74 + 10*U) * SIN(3*L0) +
+	(320 - 4*U) * COS(3*L0) -
+	212 * SIN(4*L0);
     ET = ET1000 / 1000;
 }
